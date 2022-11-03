84ec444a99616b0f062a59a09ad1c56b1a74cde4
Snork712 <snir712@gmail.com>
2022-11-03 12:35:32 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { LogInPage } from "./pages/LogIn/LogInPage";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 function App() {
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        100: "#e19b46",
+      },
+      secondary: "#5561e1",
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <LogInPage />
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/pages/LogIn/LogInPage.jsx
@@ -0,0 +1,105 @@
+import {
+  IconButton,
+  InputGroup,
+  InputLeftElement,
+  useDisclosure,
+  useMergeRefs,
+  Box,
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Image,
+  Stack,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+import loginImg from "../../assets/img/login-img.jpg";
+import owlimg from "../../assets/img/Owl.png";
+import { useState } from "react";
+
+export const LogInPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { isOpen, onToggle } = useDisclosure();
+  const inputRef = React.useRef(null);
+  const mergeRef = useMergeRefs(inputRef);
+  const logHandler = () => {
+    // log in api action
+    // user name input: userName
+    // password input: password
+  };
+
+  const misradHandler = () => {
+    // לחיצה על הזדהות אחידה משרד החינוך
+  };
+  const onClickReveal = () => {
+    onToggle();
+    if (inputRef.current) {
+      inputRef.current.focus({
+        preventScroll: true,
+      });
+    }
+  };
+  return (
+    <Flex h="100vh" alignItems="center">
+      <Box
+        w={["0", "60%", "60%"]}
+        h="100%"
+        bgImage={loginImg}
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        backgroundSize="cover"
+      ></Box>
+      <Container maxW="lg">
+        <Stack spacing="8">
+          <Stack spacing="5">
+            <FormControl>
+              <FormLabel htmlFor="username">שם משתמש</FormLabel>
+              <Input
+                id="username"
+                type="username"
+                onChange={(event) => setUserName(event.currentTarget.value)}
+                value={userName}
+              />
+              <FormLabel htmlFor="password">סיסמה</FormLabel>
+              <InputGroup>
+                <InputLeftElement>
+                  <IconButton
+                    variant="link"
+                    aria-label={isOpen ? "Mask password" : "Reveal password"}
+                    icon={isOpen ? <HiEyeOff /> : <HiEye />}
+                    onClick={onClickReveal}
+                  />
+                </InputLeftElement>
+                <Input
+                  id="password"
+                  ref={mergeRef}
+                  name="password"
+                  type={isOpen ? "text" : "password"}
+                  autoComplete="current-password"
+                  p="4"
+                  pl="10"
+                  onChange={(event) => setPassword(event.currentTarget.value)}
+                  value={password}
+                  required
+                />
+              </InputGroup>{" "}
+            </FormControl>
+          </Stack>
+          <Stack spacing="3">
+            <Button onClick={() => logHandler()} variant="solid" on>
+              התחבר
+            </Button>
+            <Button onClick={() => misradHandler()} variant="outline" size="lg">
+              <Image h="90%" src={owlimg} />
+              הזדהות אחידה משרד החינוך
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </Flex>
+  );
+};
